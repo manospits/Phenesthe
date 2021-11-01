4a92c7e34bab751d9a48ee0f3493802b508fdeb8
Emmanouil Pitsikalis <e.pitsikalis@liverpool.ac.uk>
2021-11-01 11:57:20 +0000

    cleaned the folders

--- samples/maritime-LOPSTR/run_experiments.sh
+++ samples/maritime-LOPSTR/run_experiments.sh
@@ -4,7 +4,8 @@ END=1459461588
 WINDOWS="7200 14400 28800 57600 115200"
 STEP=7200
 for WINDOW in $WINDOWS; do 
-    #ionice -c 2 -n 0 nice -n -20 swipl -O -s ./run.prolog -g "queries_on_fstream('BREST_phenesthe_input.csv','log$WINDOW.csv','results_serial$WINDOW.csv',$START,$END,$STEP,$WINDOW)." -g "halt."
-    #taskpolicy -d important swipl -O -s ./run.prolog -g "queries_on_fstream('BREST_phenesthe_input.csv','log_serial$WINDOW.csv','results_serial$WINDOW.csv',$START,$END,$STEP,$WINDOW)." -g "halt."
-    taskpolicy -d important swipl -O -s ./run.prolog -g "queries_on_fstream('BREST_phenesthe_input.csv','log$WINDOW.csv','results$WINDOW.csv',$START,$END,$STEP,$WINDOW)." -g "halt."
+    #if in mac
+    #taskpolicy -d important swipl -O -s ./run.prolog -g "queries_on_fstream('BREST_phenesthe_input.csv','logs/log$WINDOW.csv','results/results$WINDOW.csv',$START,$END,$STEP,$WINDOW)." -g "halt."
+    #if in GNU/Linux
+    ionice -c 2 -n 0 nice -n -20 swipl -O -s ./run.prolog -g "queries_on_fstream('BREST_phenesthe_input.csv','logs/log$WINDOW.csv','results/results$WINDOW.csv',$START,$END,$STEP,$WINDOW)." -g "halt."
 done
